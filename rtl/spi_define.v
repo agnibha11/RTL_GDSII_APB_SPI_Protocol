bda2b1567c94fe6df8898678f0f0dde21e6839ac
Agnibha Sarkar <agnibhasarkar11@gmail.com>
2026-06-23 15:40:33 +0530

    First Commit

---
+++ rtl/spi_define.v
@@ -0,0 +1,4 @@
+`define SPI_DIVIDER_LEN 16; //thus divider value can be from 0 to 65535
+`define SPI_MAX_CHARS 128; //Maximum 128 bits can be transferred in single transaction
+`define SPI_SS_MAX 32; //Maximum number of Slave Signals
+ 
